0ad79ede2a8bfc6b1191bc6df32e7e766e4d0cfa
puiminh11122001 <volmicrolab@gmail.com>
2022-09-13 00:16:37 +0700

    a bit logic for username

--- username.js
+++ username.js
@@ -1,9 +1,20 @@
 if(localStorage.getItem('randomtag-username') !== null) 
-    document.getElementById("user-name").textContent = localStorage.getItem('randomtag-username')
+    document.getElementById("user-name").textContent = localStorage.getItem('randomtag-username') //nguoi dung luu
 
 document.getElementById("user-name").addEventListener('input', (e) => {
     console.log(e);
-    let newName = document.getElementById("user-name").textContent;
-    console.log(newName);
-    localStorage.setItem('randomtag-username', newName);
+    let newName;
+    window.onkeyup = (e)=>{
+        switch (e.keyCode) {
+            case 13:
+                console.log("enter");
+                newName = document.getElementById("user-name").textContent = localStorage.getItem('randomtag-username');
+                return;
+            default:
+                newName = document.getElementById("user-name").textContent;
+                console.log(newName);
+                localStorage.setItem('randomtag-username', newName);
+                break;
+        } 
+    }
 });
