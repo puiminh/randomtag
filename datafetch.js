50e8e53bcf2eb3090fcf7047bba3975a73eed80c
puiminh11122001 <volmicrolab@gmail.com>
2022-09-29 23:25:32 +0700

    a bit trouble with promise

--- datafetch.js
+++ datafetch.js
@@ -14,21 +14,47 @@
 // Init a bigData array to push new data on each iteration
 let bigData = [];
 let bigDataSend = [];
+let array = [];
+
+async function fetchWiki(keyword) {
+    try {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        let fetchWikiURL = `https://capi-v2.sankakucomplex.com/tag-and-wiki/name/${keyword}`;
+        const response = await axios.get(fetchWikiURL);
+        const { data } = response;
+        return data.wiki.body;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+async function fetchArrayWiki(data) {
+    const arrayWiki = await Promise.all(data.map((tag)=> {
+            fetchWiki(tag.name_en).then((res) => {
+                let newObj = {
+                    wiki: res,
+                    ...tag
+                }
+                console.log(tag.name_en,newObj)
+                return newObj    
+            })
+    }))
+    return arrayWiki;
+}
+
+
 async function fetchByExcel(
     pageKey = 0 /** init by default page index 0 */,
 ) {
     try {
-        const fetchURL = `https://capi-v2.sankakucomplex.com/tags?lang=en&page=${pageKey}`;
+        const fetchURL = `https://capi-v2.sankakucomplex.com/tags?lang=en&rating=s&page=${pageKey}`;
         const response = await axios.get(fetchURL);
         const { data } = response; // Your api should give you a total page count, result or something to setup your iteration
-        let arrayTag = data.map(({ name }) => name)
-        let array = data.map(({ name_en,id,count,type,rating }) => {
-            let fetchWikiURL = `https://capi-v2.sankakucomplex.com/tag-and-wiki/name/${name_en}`;
-            const response = await axios.get(fetchURL);
-            return {id: id, name: name_en, count: count, rating: rating, type: type}
-        })
-        console.log(arrayTag);
-        bigData = bigData.concat(arrayTag); // push on big data response data
+        // let arrayTag = data.map(({ name }) => name)
+        array = await fetchArrayWiki(data).then((res) => {console.log(res)});
+        console.log(array);
+        // bigData = bigData.concat(arrayTag); // push on big data response data
 
         // if current page isn't the last, call the fetch feature again, with page + 1
         if (
@@ -36,10 +62,6 @@ async function fetchByExcel(
         ) {
             pageKey++;
             await new Promise((resolve) => setTimeout(resolve, 1000)); // setup a sleep depend your api request/second requirement.
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", 'https://sheetdb.io/api/v1/qpx225q4q80x1', true);
-            xhr.setRequestHeader('Content-Type', 'application/json');
-            xhr.send(JSON.stringify(array));
             return await fetchByExcel(pageKey);
         }
         return console.info('Data complete.');
@@ -49,7 +71,7 @@ async function fetchByExcel(
     }
 }
 
-fetchByExcel().then(() => {console.log(bigData); console.table(bigData);
+fetchByExcel().then(() => {console.log(array); console.log(bigData); console.table(bigData);
 });  
 
 
